ebcbc6bec63a5f4c78438850afc636daa6db9da9
DaniiMello <daniellemello.p@gmail.com>
2022-08-30 12:52:58 -0300

    Exercício1

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+let numero = prompt ("Digite um número par")
+typeof numero // number
+console.log (`
+O resto da divisão ${numero%2}
+`)
+//Todo número par dividido por 2, seu resultado é zero.
+//Enquanto todo número impar dividido por 2, o resultadoserá 1.
